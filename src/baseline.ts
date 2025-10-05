d582736c1244a6c770a8b1cab8366d85e3370f31
samarth3301 <samarplayz1337@gmail.com>
2025-10-05 14:20:32 +0530

    Implement code changes to enhance functionality and improve performance

--- src/baseline.ts
+++ src/baseline.ts
@@ -1,5 +1,5 @@
 import { features } from "web-features";
-import { baseline } from "compute-baseline";
+import * as computeBaselinePkg from "compute-baseline";
 import { BaselineInfo, BaselineStatus } from "./@types/scanner";
 
 
@@ -64,7 +64,7 @@ async function bcdCheck(api: string): Promise<BaselineInfo | null> {
     const bcdKey = mapApiToBCDKey(api);
     if (!bcdKey) {return null;}
 
-    const status = baseline.computeBaseline({ compatKeys: [bcdKey] });
+    const status = computeBaselinePkg.baseline.computeBaseline({ compatKeys: [bcdKey] });
 
     if (status) {
       const support: Record<string, string> = {};
